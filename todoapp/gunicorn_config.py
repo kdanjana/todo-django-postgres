3df9d1a552ed438c75fa8dfeb4162fa96aefac51
anjana <kad.an.devi@gmail.com>
2023-10-13 15:41:03 +0530

    added gunicorn config

---
+++ todoapp/gunicorn_config.py
@@ -0,0 +1,3 @@
+workers = 4              # Adjust the number of workers as needed
+bind = "0.0.0.0:8000"    # The IP and port to bind to
+timeout = 120            # Increase the timeout as needed for long-running requests
