244a5e8158202799d394e6f00d04457454b71bf2
anjana <kad.an.devi@gmail.com>
2023-10-13 15:40:01 +0530

    added tests

--- todoapp/coreapp/tests.py
+++ todoapp/coreapp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
+from coreapp.models import Task
+
 # Create your tests here.
+
+class TasTestCase(TestCase):
+    
+    def setUp(self):
+        """ set up necessary data for tests"""
+        Task.objects.create(title="test1")
+
+    def test_is_important(self):
+        test_task = Task.objects.get(title="test1")
+        self.assertEqual(test_task.is_important, "False")
