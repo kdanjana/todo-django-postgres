9b6b0fdd113836770e21dc8ed1f018292f37a6f8
anjana <kad.an.devi@gmail.com>
2023-10-16 11:59:44 +0530

    added tests

--- todoapp/coreapp/tests.py
+++ todoapp/coreapp/tests.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from coreapp.models import Task
+from coreapp import views
 
 # Create your tests here.
 
-class TasTestCase(TestCase):
+class TaskModelTestCase(TestCase):
     
     def setUp(self):
         """ set up necessary data for tests"""
@@ -13,3 +15,20 @@ class TasTestCase(TestCase):
     def test_is_important(self):
         test_task = Task.objects.get(title="test1")
         self.assertEqual(test_task.is_important, "False")
+
+
+      
+class ViewsTestCase(TestCase):
+    def setUp(self):
+        # Set up any necessary data for the tests
+        Task.objects.create(title="test1")
+
+    def test_imptasks_response(self):
+        # Test the view's response
+        response = self.client.get(reverse('imp-tasks'))
+        self.assertEqual(response.status_code, 200)
+
+    # def test_view_context(self):
+    #     # Test the context of the view
+    #     response = self.client.get(reverse('your-view-name'))
+    #     self.assertEqual(response.context['your_variable'], "Expected Value")
